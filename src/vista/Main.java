8278c89966b0c11af739e52ef84eaf9458ebdbd3
GabyValladares <126087266+GabyValladares@users.noreply.github.com>
2024-05-21 17:03:04 -0500

    Main

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -4,10 +4,29 @@
  */
 package vista;
 
+import java.util.Scanner;
+import modelo.Cliente;
+
 /**
  *
  * @author Usuario
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner es=new Scanner(System.in);
+        Cliente[] listaClientes=new Cliente[10];
+        for (int i = 0; i < 10; i++) {
+            Cliente c=new Cliente();
+            System.out.println("---INFORMACIÓN CLIENTE---");
+            System.out.println("Ingrese el código del Cliente:");
+            c.setGgvcIdCliente(es.nextInt());
+            System.out.println("Ingrese el nombre del Cliente:");
+            c.setGgvcNombres(es.next());
+            listaClientes[i]=c;
+            
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(listaClientes[i].getGgvcNombres());
+        }
+    }
 }
